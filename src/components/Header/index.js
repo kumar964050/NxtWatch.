02c56513efad614913cd337d17489f30b59b00af
prasanth <prasanth@example.com>
2023-03-10 07:22:13 +0000

    Header Added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,100 @@
 import {Component} from 'react'
+import Popup from 'reactjs-popup'
 import {FaMoon} from 'react-icons/fa'
-import {HeaderContainer, HeaderImage, HeaderTheme} from './styledComponent'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {FiLogOut} from 'react-icons/fi'
+import {AiFillHome} from 'react-icons/ai'
+import {HiFire} from 'react-icons/hi'
+import {SiYoutubegaming} from 'react-icons/si'
+import {MdPlaylistAdd} from 'react-icons/md'
+import {
+  HeaderContainer,
+  HeaderImage,
+  HeaderTheme,
+  HamburgerButton,
+  LogoutButton,
+  HeaderThemeContainer,
+  ReactNavIcons,
+  ListItem,
+  UnorderedList,
+  NavNames,
+} from './styledComponent'
 import ThemeContext from '../../context/ThemeContext'
+import './index.css'
 
 class Header extends Component {
+  state = {
+    hamburger: false,
+  }
+
+  openHamburger = () => {
+    this.setState(prevState => ({hamburger: !prevState.hamburger}))
+  }
+
   render() {
+    const {hamburger} = this.state
     return (
-      <ThemeContext.Consumer>
-        {value => {
-          const {darkTheme, changeTheme} = value
-          const changeAppTheme = () => {
-            changeTheme()
-          }
+      <>
+        <ThemeContext.Consumer>
+          {value => {
+            const {darkTheme, changeTheme} = value
+            const changeAppTheme = () => {
+              changeTheme()
+            }
 
-          const headerLogo = darkTheme
-            ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
-            : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
+            const headerLogo = darkTheme
+              ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
+              : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
 
-          return (
-            <HeaderContainer>
-              <HeaderImage src={headerLogo} />
-              <HeaderTheme type="button" onClick={changeAppTheme}>
-                <FaMoon />
-              </HeaderTheme>
-            </HeaderContainer>
-          )
-        }}
-      </ThemeContext.Consumer>
+            return (
+              <HeaderContainer>
+                <HeaderImage src={headerLogo} />
+                <HeaderThemeContainer>
+                  <HeaderTheme type="button" onClick={changeAppTheme}>
+                    <FaMoon className="dark-theme" />
+                  </HeaderTheme>
+
+                  <HamburgerButton type="button" onClick={this.openHamburger}>
+                    <GiHamburgerMenu className="dark-theme" />
+                  </HamburgerButton>
+                  <LogoutButton>
+                    <FiLogOut className="dark-theme" />
+                  </LogoutButton>
+                </HeaderThemeContainer>
+              </HeaderContainer>
+            )
+          }}
+        </ThemeContext.Consumer>
+
+        {hamburger && (
+          <UnorderedList>
+            <ListItem>
+              <ReactNavIcons>
+                <AiFillHome />
+              </ReactNavIcons>
+              <NavNames>Home</NavNames>
+            </ListItem>
+            <ListItem>
+              <ReactNavIcons>
+                <HiFire />
+              </ReactNavIcons>
+              <NavNames>Trending</NavNames>
+            </ListItem>
+            <ListItem>
+              <ReactNavIcons>
+                <SiYoutubegaming />
+              </ReactNavIcons>
+              <NavNames>Gaming</NavNames>
+            </ListItem>
+            <ListItem>
+              <ReactNavIcons>
+                <MdPlaylistAdd />
+              </ReactNavIcons>
+              <NavNames>Saved Videos</NavNames>
+            </ListItem>
+          </UnorderedList>
+        )}
+      </>
     )
   }
 }
--- src/components/Header/styledComponent.js
+++ src/components/Header/styledComponent.js
@@ -2,12 +2,64 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.div`
   height: 75px;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 5%;
+  align-items: center;
 `
 
 export const HeaderImage = styled.img`
+  height: 30px;
+`
+
+export const HeaderThemeContainer = styled.div`
   height: 40px;
 `
 
 export const HeaderTheme = styled.button`
+  height: 30px;
   background: transparent;
+  border: 0px;
+  cursor: pointer;
+  outline: none;
+`
+
+export const UnorderedList = styled.ul`
+  list-style-type: none;
+  padding-left: 0px;
+  width: 200px;
+  margin-top: 71px;
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  border-radius: 6px;
+  margin-right: 2%;
+`
+
+export const HamburgerButton = styled(HeaderTheme)`
+  height: 40px;
+`
+export const LogoutButton = styled(HeaderTheme)`
+  height: 40px;
+`
+
+export const ListItem = styled.li`
+  padding-left: 30px;
+  height: 45px;
+  padding-right: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+export const NavNames = styled.p`
+  font-family: 'Roboto';
+  color: #475569;
+  font-weight: 400;
+`
+export const ReactNavIcons = styled.div`
+  height: 20px;
+  color: #475569;
+`
+export const UnOrderedListCon = styled.div`
+  height: 180px;
 `
